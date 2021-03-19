f520d2360ef29a98f2485dd4bf83448818ba8bd1
Mirza <mirza.hamurko@gmail.com>
2021-03-19 23:28:21 +0100

    ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,25 @@
+// Only change code below this line
+function sumFibonacci(num) {
+    if (num == 1) {
+      return 1;
+    }
+    var prevNumber = 0;
+    var currNumber = 1;
+    var result = 0;
+    while (currNumber <= num) {
+      if (currNumber % 2 !== 0) {
+        result += currNumber;
+      }
+      currNumber += prevNumber;
+      prevNumber = currNumber - prevNumber;
+    }
+    return result;
+  }
+  // Only change code above this line
+  console.log(sumFibonacci(1));
+  console.log(sumFibonacci(10));
+  console.log(sumFibonacci(20)); //Change this line
+  console.log(sumFibonacci(4));
+  console.log(sumFibonacci(-5));
+  module.exports = sumFibonacci;
+  //return  module.exports;
